e81e148f8fac4e53ada6174abca387463bf590cb
huanglin233 <www.2512467648@qq.com>
2024-05-23 00:06:39 +0800

    node测试相关

--- fullstackopen-api/utils/config.js
+++ fullstackopen-api/utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT;
-const MONGODB_URL = process.env.DB_URL;
+const MONGODB_URL = process.env.NODE_ENV === 'test' ? process.env.DB_URL_TEST : process.env.DB_URL;
 
 module.exports = {
     MONGODB_URL,
